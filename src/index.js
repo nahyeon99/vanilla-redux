523a6c891b4e8d48d1b9c7b0d7cad2aae2e04604
NaHyeon <nahyeonee99@gmail.com>
2021-04-26 18:12:19 +0900

    #1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
-  if (action.type === "ADD") {
+  if (action.type === "add") {
     return count + 1;
-  } else if (action.type === "MINUS") {
+  } else if (action.type === "minus") {
     return count - 1;
   } else {
     return count;
@@ -17,11 +18,11 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
-console.log(countStore.getState());
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({ type: "add" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "minus" }));
