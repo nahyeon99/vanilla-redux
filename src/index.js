061c6b926683014c0609853ed57dd4755c645533
NaHyeon <nahyeonee99@gmail.com>
2021-04-26 18:23:39 +0900

    #1.4 Recap Refactor

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "add";
+const MINUS = "minus";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "add") {
-    return count + 1;
-  } else if (action.type === "minus") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -24,5 +28,5 @@ const onChange = () => {
 
 countStore.subscribe(onChange);
 
-add.addEventListener("click", () => countStore.dispatch({ type: "add" }));
-minus.addEventListener("click", () => countStore.dispatch({ type: "minus" }));
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
